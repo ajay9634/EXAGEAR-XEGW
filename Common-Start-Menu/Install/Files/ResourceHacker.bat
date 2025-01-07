437e789934b53fff622dc56d86400cbffddb8fcd
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-07 14:34:18 +0530

    Add files via upload

---
+++ Common-Start-Menu/Install/Files/ResourceHacker.bat
@@ -0,0 +1,24 @@
+@echo off
+color 0a
+echo *** Enjoy flash or swf games ***
+echo *** script made by Ajay ***
+
+:: Simulating bold with color and emphasis
+color 0A
+echo.
+echo *** Downloading ResourceHacker ***
+IF NOT EXIST "Z:\opt\wget_files\Files\ResourceHacker.7z" (
+    wget -P Z:/opt/wget_files/temp/ --progress=dot:mega https://raw.githubusercontent.com/ajay9634/Ajay-prefix/Resources/My-files/ResourceHacker.7z
+    copy /s /y Z:\opt\wget_files\temp\ResourceHacker.7z Z:\opt\wget_files\Files\ResourceHacker.7z /E /H /C /I
+) ELSE (
+    ECHO ResourceHacker already exists.
+)
+
+
+color 1f
+echo *** Extracting ...***
+Z:\opt\7z.exe x Z:\opt\wget_files\Files\ResourceHacker.7z -oZ:\opt\wget_files\temp\ -r -y >NUL 2>&1
+
+echo *** opening ResourceHacker ***
+Start Z:\opt\wget_files\temp\ResourceHacker.exe
+echo ************************************************
