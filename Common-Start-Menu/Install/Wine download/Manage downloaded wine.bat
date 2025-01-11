a5a8874e2bcd00f11b1246cb3330b3c694199b51
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-11 21:24:14 +0530

    Create Manage downloaded wine.bat

---
+++ Common-Start-Menu/Install/Wine download/Manage downloaded wine.bat
@@ -0,0 +1,6 @@
+@echo off
+echo delete downloaded wines manually but don't delete wine-4.0.4 and wine-8.2.
+taskkill /f /im 7zFM.exe >NUL 2>taskkill /f /im 7zFM.exe >NUL 2>&1
+rmdir /S /Q Z:\opt\7-Zip\temp\
+
+Z:\opt\7z.exe x Z:\opt\7-Zip\7-Zip.7z -oZ:\opt\7-Zip\temp  -p1 -r -y >NUL 2>&1
