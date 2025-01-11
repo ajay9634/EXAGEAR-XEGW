7f1fc3659f5578b4fbcce5bc45037472ed8f4f00
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-11 21:27:28 +0530

    Update Manage downloaded wine.bat

--- Common-Start-Menu/Install/Wine download/Manage downloaded wine.bat
+++ Common-Start-Menu/Install/Wine download/Manage downloaded wine.bat
@@ -4,3 +4,4 @@ taskkill /f /im 7zFM.exe >NUL 2>taskkill /f /im 7zFM.exe >NUL 2>&1
 rmdir /S /Q Z:\opt\7-Zip\temp\
 
 Z:\opt\7z.exe x Z:\opt\7-Zip\7-Zip.7z -oZ:\opt\7-Zip\temp  -p1 -r -y >NUL 2>&1
+start "" "Z:\opt\7-Zip\temp\7zFM.exe" "Z:\opt\wineCollection\custom"
