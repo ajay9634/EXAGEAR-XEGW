d720ad97ec397da0d836a94f9686feefbf5f8ac8
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-01-30 13:14:37 +0530

    Update and rename wined3d-10.0-rc4.bat to wined3d-10.0.bat

--- Common-Start-Menu/Install/d3d/wined3d(universal)/wined3d-10.0.bat
+++ Common-Start-Menu/Install/d3d/wined3d(universal)/wined3d-10.0.bat
@@ -13,7 +13,7 @@ color 0A
 echo.
 echo *** Downloading %installname% ***
 IF NOT EXIST "Z:/opt/wget_files/wined3d/%installname%.zip" (
-    wget -O Z:/opt/wget_files/temp/%installname%.zip https://downloads.fdossena.com/Projects/WineD3D/Builds/WineD3DForWindows_10.0-rc4.zip
+    wget -O Z:/opt/wget_files/temp/%installname%.zip https://downloads.fdossena.com/Projects/WineD3D/Builds/WineD3DForWindows_10.zip
     copy "Z:/opt/wget_files/temp/%installname%.zip" "Z:/opt/wget_files/wined3d/%installname%.zip"
     echo Extracting....
     Z:\opt\winrar.exe x Z:\opt\wget_files\wined3d\%installname%.zip Z:\opt\wget_files\temp\ -r -y >NUL 2>&1
