5090be14aabce4e5f08bb39d7d03207a78cab013
Ajay <78233144+ajay9634@users.noreply.github.com>
2025-04-04 19:19:11 +0530

    Update dxwrapper-1.2.7500.25.bat

--- Common-Start-Menu/Install/d3d/dxvk(snapdragon)/dxwrapper-1.2.7500.25.bat
+++ Common-Start-Menu/Install/d3d/dxvk(snapdragon)/dxwrapper-1.2.7500.25.bat
@@ -4,8 +4,9 @@ set "installname=dxwrapper-1.2.7500.25"
 color 0a
 echo *** wait....***
 echo *** deleting temp files...***
-rmdir /S /Q "Z:/opt/wget_files/temp"
-mkdir "Z:/opt/wget_files/temp"
+rmdir /S /Q "Z:/opt/wget_files/temp" >NUL 2>&1
+mkdir "Z:/opt/wget_files/temp" >NUL 2>&1
+mkdir "Z:\opt\wget_files\wined3d" >NUL 2>&1
 echo *** deleted temp files ***
 echo .
 echo *** script made by Ajay ***
